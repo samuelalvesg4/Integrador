f1b0fdab88f2835a11ae9d0e7151fe6120f977eb
samuelalvesg4 <134507482+samuelalvesg4@users.noreply.github.com>
2025-09-18 16:24:42 -0300

    Update App.jsx

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -9,55 +9,59 @@ import CreateProduct from "./pages/CreateProduct";
 import RegisterProduct from "./pages/RegisterProduct";
 import MyProducts from "./pages/MyProducts";
 import Sales from "./pages/Sales";
+import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* Rotas públicas */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
+      {/* O CartProvider deve envolver todas as rotas */}
+      <CartProvider>
+        <Routes>
+          {/* Rotas públicas */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
 
-        {/* Rotas protegidas para clientes */}
-        <Route path="/product/:id" element={<ProductDetail />} />
-        <Route
-          path="/cart"
-          element={
-            <ProtectedRoute>
-              <Cart />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/create-product" element={<CreateProduct />} />
-        
+          {/* Rotas protegidas para clientes */}
+          <Route
+            path="/cart"
+            element={
+              <ProtectedRoute>
+                <Cart />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/create-product" element={<CreateProduct />} />
+          
 
-        {/* Rotas protegidas apenas para Vendedores */}
-        <Route
-          path="/RegisterProduct"
-          element={
-            <ProtectedRoute role="seller">
-              <RegisterProduct />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/MyProducts"
-          element={
-            <ProtectedRoute role="seller">
-              <MyProducts />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/Sales"
-          element={
-            <ProtectedRoute role="seller">
-              <Sales />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+          {/* Rotas protegidas apenas para Vendedores */}
+          <Route
+            path="/RegisterProduct"
+            element={
+              <ProtectedRoute role="seller">
+                <RegisterProduct />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/MyProducts"
+            element={
+              <ProtectedRoute role="seller">
+                <MyProducts />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/Sales"
+            element={
+              <ProtectedRoute role="seller">
+                <Sales />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </CartProvider>
     </Router>
   );
 }
